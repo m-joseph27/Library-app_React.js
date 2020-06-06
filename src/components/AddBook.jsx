433c29400d9ea23e9e440c1540b619d0e42ef515
m-joseph27 <muh.joseph27@gmail.com>
2020-06-06 21:32:20 +0700

    finish add book component

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -8,9 +8,26 @@ class AddBook extends Component {
         <div className="AddBookWrapper">
           <div className="addBook">
             <div className="headerBook">
+              <h2>Add Book</h2>
+              <i class="fas fa-times"></i>
             </div>
-            <div className="bodyBook"></div>
-            <div className="footerBook"></div>
+            <div className="bodyBook">
+              <div className="leftsideBook">
+                <ul>
+                  <li>URL Image</li>
+                  <li>Title</li>
+                  <li>Description</li>
+                </ul>
+              </div>
+              <div className="rightsideBook">
+              <input type="text"></input>
+              <input type="text"></input>
+              <textarea></textarea>
+              </div>
+            </div>
+            <div className="footerBook">
+              <button>Save</button>
+            </div>
           </div>
         </div>
       </>
