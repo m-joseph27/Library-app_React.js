24bac42a871ba35e30e86c125a4babcbe32e0a33
m-joseph27 <muh.joseph27@gmail.com>
2020-06-08 20:07:45 +0700

    edit navbar

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,21 @@
 import React, {Component} from 'react'
 import "../scss/Detail.scss"
+import Arrow from "../assets/img/left-arrow.png"
 
 class Detail extends Component   {
   render() {
     return(
       <>
         <div className="detailwrapper">
-          <div className="navDetail"></div>
+          <div className="navDetail">
+            <div className="navArrow"><img src={Arrow} alt="arrow"></img></div>
+            <div className="listEdit">
+              <ul>
+              <li>Edit</li>
+              <li>Delete</li>
+              </ul>
+            </div>
+          </div>
           <div className="jumbotronDetail"></div>
           <div className="detailBook"></div>
         </div>
