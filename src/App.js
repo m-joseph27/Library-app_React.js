ad706dcb58ac21b44f7066f5a8e642dafa8fadf2
m-joseph27 <muh.joseph27@gmail.com>
2020-06-11 19:54:41 +0700

    modified navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
-// import Navbar from './components/Navbar'
+import Navbar from './components/Navbar'
 // import Login from './pages/Login'
-import Detail from "./pages/Detail"
+// import Detail from "./pages/Detail"
 
 class App extends Component{
   render(){
     return(
       <>
-      <Detail/>
+      <Navbar/>
       </>
     )
   }
